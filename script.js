a7a7d4e83235dca17485f700f895061a1b4569da
arslonga83 <jlwood83@gmail.com>
2022-09-29 15:22:57 -0700

    cleanup code

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ const code = '495619d270c09909b5c5a53a12eb4121';
 const input = document.querySelector('input');
 const button = document.querySelector('button');
 const body = document.querySelector('body');
+const h1 = document.getElementById('line1');
+const h2 = document.getElementById('line2');
+const desc = document.getElementById('line3');
 
 button.addEventListener('click', () => {
   let location = input.value.split(', ');
@@ -10,29 +13,31 @@ button.addEventListener('click', () => {
 })
 
 const getGeoLocation = async function (city, state) {
+  try {
   const response = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${city},${state},{country code}&limit=1&appid=${code}`);
   const data = await response.json();
   getWeather(data[0].lat, data[0].lon);
+  } catch (err) {
+    printForecast('Oops...', '', 'something went wrong...')
+  }
 }
 
 const getWeather = async function (lat, lon) {
+  try {
   const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${code}&units=imperial`)
   const weather = await response.json();
   printForecast(weather.name, weather.main.temp, weather.weather[0].description);
   displayBackground(weather.weather[0].main);
   console.log(weather);
-  console.log(`${weather.main.temp} deg F`);
-  console.log(weather.weather[0].description)
+  } catch (err) {
+    printForecast('Oops', '', 'something went wrong...')
+  }
 }
 
 const forecast = document.getElementById('forecast');
 const printForecast = (city, temp, description) => {
-  forecast.innerHTML = '';
-  const h1 = forecast.appendChild(document.createElement('h1'));
   h1.textContent = city;
-  const h2 = forecast.appendChild(document.createElement('h2'));
   h2.textContent = `${temp}°F`;
-  const desc = forecast.appendChild(document.createElement('h2'));
   desc.textContent = description;
 }
 
@@ -53,9 +58,6 @@ const displayBackground = (weather) => {
   body.style.backgroundSize = 'cover';
 }
 
-
-
-
-
+//call local weather on page load
 getGeoLocation('portland', 'or');
 
